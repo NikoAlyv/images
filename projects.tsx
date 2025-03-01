c49482bb3017b5df288d251ab67ac305f3ee2309
NikoAlyv <enihad801@gmail.com>
2025-03-01 11:28:06 +0400

    change image

--- projects.tsx
+++ projects.tsx
@@ -1,14 +1,16 @@
 [
   {
     id: 1,
-    image: require("../assets/images/project1.png"),
+    image:
+      "https://raw.githubusercontent.com/NikoAlyv/images/refs/heads/main/project1.png",
     title: "Master English",
     description: `Master English is a mobile application designed to help users improve their English grammar and vocabulary through interactive learning. The app includes key features such as grammar and quizzes, allowing users to learn English grammar through topic-based interactive quizzes. It offers level-based vocabulary, providing access to essential words categorized from A1 to C2 levels with usage examples. AI-powered explanations generate detailed answers and explanations for grammar topics. Flashcards and pronunciation support enhance vocabulary retention, allowing users to listen to correct pronunciations. The app features a user-friendly and modern design with a clean, minimalist interface for an engaging learning experience.`,
     link: "https://play.google.com/store/apps/details?id=com.mastersenglish&pcampaignid=web_share",
   },
   {
     id: 2,
-    image: require("../assets/images/project2.png"),
+    image:
+      "https://raw.githubusercontent.com/NikoAlyv/images/refs/heads/main/project2.png",
     title: "Shoppay",
     description:
       "Shoppay is a modern e-commerce mobile application that provides users with a seamless and intuitive shopping experience. Designed for both iOS and Android, the app ensures a native feel on each platform while offering powerful features such as user registration and authentication for secure access, advanced search and filtering options to find products easily, a smooth shopping experience with an intuitive cart system, a cross-platform optimized UI for a fluid experience on both iOS and Android, and a fast and secure checkout process with multiple payment options. Shoppay brings efficiency and convenience to online shopping, making it a go-to solution for modern buyers.",
@@ -16,7 +18,8 @@
   },
   {
     id: 3,
-    image: require("../assets/images/project3.png"),
+    image:
+      "https://raw.githubusercontent.com/NikoAlyv/images/refs/heads/main/project3.png",
     title: "Apple Snake",
     description:
       "Apple Snake is a classic snake game designed with a modern touch. It marks my first experience publishing an app on the Play Store. The game offers smooth controls and dynamic gameplay, allowing players to enjoy features such as color customization and speed adjustments. With a simple yet engaging design, Apple Snake provides an entertaining and interactive experience for users of all ages.",
@@ -24,7 +27,8 @@
   },
   {
     id: 4,
-    image: require("../assets/images/project4.png"),
+    image:
+      "https://raw.githubusercontent.com/NikoAlyv/images/refs/heads/main/project4.png",
     title: "Attack Protection",
     description:
       "This application helps users detect social engineering attacks within texts. Its primary goal is to identify various social engineering techniques, from phishing to fear induction attacks, and alert users about potential threats.In addition to detection, the app provides educational resources on social engineering attacks. This allows users not only to recognize potential threats but also to enhance their knowledge of different attack techniques.Designed both for protection and awareness, the application analyzes text using specific keywords to determine if it contains potentially harmful content. It can identify ten different types of social engineering attacks, including phishing, fear induction, spam and fraudulent offers, spear phishing, and baiting. For each type of attack, the app provides customized warning messages to keep users informed and protected",
@@ -32,7 +36,8 @@
   },
   {
     id: 5,
-    image: require("../assets/images/project3.png"),
+    image:
+      "https://raw.githubusercontent.com/NikoAlyv/images/refs/heads/main/project4.png",
     title: "ChatBot",
     description:
       "ChatBot is the first chatbot application I developed using Gemini AI. It provides an interactive and intelligent conversational experience, allowing users to engage in dynamic chats powered by advanced AI. The app also includes customization features, such as the ability to select text colors, enhancing the user experience with a personalized touch",
